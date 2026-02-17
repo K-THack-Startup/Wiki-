d7a87c0771168a3c656a5aa96b2844aa77b55328
DanilKhasanov <danilhasanov572@gmail.com>
2026-02-17 22:36:40 +0500

    registration-endpoints

--- Backend/расписание/KDT_HACK/src/main/java/com/hackathon/KDT_HACK/ScheduleRepository.java
+++
@@ -1,6 +0,0 @@
-package com.hackathon.KDT_HACK;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ScheduleRepository extends JpaRepository<ScheduleEntity, Long> {
-}
